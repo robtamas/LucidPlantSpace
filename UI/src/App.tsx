c9b4187c40dabe2bb49adadfcb8a3f52f3d9aa56
Andrew Pittaway <apitaway@assessor.com.au>
2025-02-20 14:16:33 +1100

    Demo upload and assessment

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
+import FileUpload from "./FileUpload";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" element={<h1>MADHAT</h1>} />
           <Route path="/page2" element={<h2>MADHAT</h2>} />
           <Route path="/page3" element={<h3>MADHAT</h3>} />
+          <Route path="/upload" element={<FileUpload />} />
         </Route>
       </Routes>
     </BrowserRouter>
