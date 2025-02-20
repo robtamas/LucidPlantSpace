7692107cdb6296a953445ca12b3aae6cd223f7ac
Andrew Pittaway <apitaway@assessor.com.au>
2025-02-20 13:28:18 +1100

    Basic routes

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 
 function App() {
   return (
-    <>
-      <h1>Most awesomeness hazard detection tool</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<h1>MADHAT</h1>} />
+          <Route path="/page2" element={<h2>MADHAT</h2>} />
+          <Route path="/page3" element={<h3>MADHAT</h3>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
